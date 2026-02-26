2dc80ce825ae34aa86edc310cfd65915ec4d86fd
Abu Nasim <info.abunasim@gmail.com>
2026-02-26 23:19:54 +0600

    Clean requirements for PythonAnywhere

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() not in ('false', '0', 'no')
 
-ALLOWED_HOSTS = ['*'] # In production, set this to actual domains like ['your-app.onrender.com']
+ALLOWED_HOSTS = ['*'] # Required for PythonAnywhere free tier
 
 
 # Application definition
